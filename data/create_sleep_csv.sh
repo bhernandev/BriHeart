3ade9e4fe629e3220e2cd7c8aa0430195cbbefe8
Brian Hernandez <brian@hernandez-cs.com>
2020-04-13 01:38:32 -0400

    small fix to create_sleep_csv.sh, update gitignore

--- data/create_sleep_csv.sh
+++ data/create_sleep_csv.sh
@@ -2,4 +2,4 @@ exec 3> $2
 
 echo "creation_date,start_date,end_date,sleep_type" >&3 
 
-grep 'HKCategoryTypeIdentifierSleepAnalysisInBed' $1 | tr -d '/<>"' | sed 's/[a-zA-Z]*=/ /g' | awk -F '  ' '{print $5","$6","$7","$8}' >&3
+grep 'HKCategoryTypeIdentifierSleepAnalysis' $1 | tr -d '/<>"' | sed 's/[a-zA-Z]*=/ /g' | awk -F "  " '{print $5","$6","$7","$8}' >&3
